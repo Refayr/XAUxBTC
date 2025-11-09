e29a3e89114211c1d12a97ce274816dd53bbf53c
Refayr <grosbob@gmail.com>
2025-11-10 00:26:11 +0100

    Plot the graph

--- XAUxBTC.py
+++ XAUxBTC.py
@@ -1,6 +1,7 @@
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 import pandas
+import matplotlib.pyplot as plt
 
 
 # Set the path to the file you'd like to load from Kaggle's dataset
@@ -134,13 +135,22 @@ dfXAU = kagglehub.load_dataset(
 )
 
 
+# We wants columns to be: "ticker", "date" (yyyy-mm-dd), "open", "high", "low", "close"
+dfXAU["date"] = dfXAU["Date"].astype(str).str[:10]
+dfXAU["ticker"] = "XAU"
+dfXAU = dfXAU.rename(
+    columns={"Open": "open", "High": "high", "Low": "low", "Close": "close"}
+)
+dfXAU = dfXAU.drop(columns=["Date", "Volume", "name"])
+
+
 # Sort values by dates
-dfXAU.sort_values(by="Date")
-dfBTC.sort_values(by="date")
+dfXAU = dfXAU.sort_values(by="date")
+dfBTC = dfBTC.sort_values(by="date")
 
 
 # Get the first common date
-firstDateGold = dfXAU["Date"].iloc[0]
+firstDateGold = dfXAU["date"].iloc[0]
 firstDateCrypto = dfBTC["date"].iloc[0]
 
 if firstDateGold > firstDateCrypto:
@@ -151,7 +161,7 @@ print(firstDate)
 
 
 # Get the last common date
-lastDateGold = dfXAU["Date"].iloc[-1]
+lastDateGold = dfXAU["date"].iloc[-1]
 lastDateCrypto = dfBTC["date"].iloc[-1]
 
 if lastDateGold < lastDateCrypto:
@@ -161,11 +171,42 @@ else:
 print(lastDate)
 
 
-# Print the columns
-print(dfXAU.columns)
-print(dfBTC.columns)
-print(dfXAU.head)
-print(dfBTC.head)
-
 # Create a new dataframe with only common columns
-df = pandas.DataFrame(data=None, index=None, columns=None, dtype=None, copy=None)
+# df = pandas.DataFrame(data=None, index=None, columns=None, dtype=None, copy=None)
+df = pandas.concat([dfBTC, dfXAU])
+df = df[df["date"] >= firstDate]
+df = df[df["date"] <= lastDate]
+df = df.dropna()
+df = df.sort_values(by="date")
+print(df.head)
+
+
+# Draw values on a graph
+xau_data = df[df["ticker"] == "XAU"]
+btc_data = df[df["ticker"] == "BTC"]
+plt.figure(figsize=(12, 6))
+plt.plot(
+    xau_data["date"],
+    xau_data["open"],
+    marker="o",
+    linewidth=2,
+    markersize=4,
+    label="XAU",
+    color="yellow",
+)
+plt.plot(
+    btc_data["date"],
+    btc_data["open"],
+    marker="s",
+    linewidth=0,
+    markersize=4,
+    label="BTC",
+    color="blue",
+)
+plt.xlabel("Date", fontsize=12)
+plt.ylabel("Open prices", fontsize=12)
+plt.title("Gold / Bitcoin comparison", fontsize=14, fontweight="bold")
+plt.legend(fontsize=11)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.show()
